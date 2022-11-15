7ea52373765e4ab39e5e88152610959a124f28be
MinhoYoo <minhoyoo1899@gmail.com>
2022-11-15 18:54:48 +0900

    a

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import pymysql
+
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return '이메시지가 나온다면 플라스크 성공!'
+    return '이 메시지가 나온다면 플라스크 성공!'
+    
 @app.route('/user/<user_name>/<int:user_id>')
 def user(user_name, user_id):
     return f'Hello, {user_name}({user_id})!'
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# conn = pymysql.connect(
+#     host='127.0.0.1',
+#     user='root',
+#     password='TheoHernandez19!',
+#     port=3306,
+#     db='aitrading_db',
+#     charset='utf-8'
+# )
+
+# cur = conn.cursor()
---
+++ db.py
@@ -0,0 +1,21 @@
+# import pymysql as maria
+
+# def select(uid, upw):
+#   row = None
+#   connnection = None
+#   try:
+
+#     connection = maria.connect(
+#       host = 'localhost',
+#       user = 'root',
+#       password = 'TheoHernandez19!',
+#       database = 'aitrading_db',
+#       port = 3306,
+#       cursorclass= maria.cursors.DictCursor
+#     )
+
+#     cursor = connection.cursor()
+
+#     sql = "SELECT * FROM 'aitrading_db'.'companylist'"
+
+    
